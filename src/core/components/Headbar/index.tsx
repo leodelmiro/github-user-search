d544cbe02b29bf88014d77bfeb3185e44e5c639f
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-10-18 09:42:38 -0300

    Headbar finished

---
+++ src/core/components/Headbar/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+const Headbar = () => (
+    <div className="headbar">
+        <h2 className="headbar-title">Bootcamp DevSuperior</h2>
+    </div>
+);
+
+export default Headbar;
