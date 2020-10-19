47ff2bc060ae59f5e39bb40de61aa88885493631
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-10-19 19:53:23 -0300

    GET from Github OK, Project Ok

--- src/pages/Search/components/Loaders/InfoLoader.tsx
+++ src/pages/Search/components/Loaders/InfoLoader.tsx
@@ -4,13 +4,13 @@ import ContentLoader from "react-content-loader"
 const InfoLoader = () => (
   <ContentLoader 
     speed={2}
-    width={870}
-    height={260}
-    viewBox="0 0 870 260"
+    width={900}
+    height={300}
+    viewBox="0 -5 900 285"
     backgroundColor="#dedede"
     foregroundColor="#f5f5f5"
   >
-    <rect x="0" y="0" rx="2" ry="2" width="870" height="260" />
+    <rect x="10" y="-25" rx="2" ry="2" width="900" height="285" />
   </ContentLoader>
 )
 
--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,80 +1,126 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../../core/components/Button';
+import { User } from '../../core/types/User';
 import CardBase from './components/CardBase';
+import ImageLoader from './components/Loaders/ImageLoader';
+import InfoLoader from './components/Loaders/InfoLoader';
 import ProfileFieldData from './components/ProfileFieldData';
 import UpperProfileData from './components/UpperProfileData';
+import axios from 'axios';
 import './styles.css'
+import dayjs from 'dayjs';
 
-const Search = () => (
-    <>
+type FormState = {
+    name: string,     
+}
+
+const BASE_URL = 'https://api.github.com/users/'
+
+
+const Search = () => {
+    const [username, setUsername] = useState<FormState>({
+        name: ''
+    });
+    const [user, setUser] = useState<User>();
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        setUsername(data => ({[name]: value}))
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        setIsLoading(true);
+        axios.get(`${BASE_URL}${username.name}`)
+            .then(response => setUser(response.data))
+            .finally(() => setIsLoading(false));
+    }
+   
+    return (
+        <>
         <div>
             <CardBase className="card-base">
                 <div className="card-base-search-profile">
                     <h1 className="find-profile-title">Encontre um perfil Github</h1>
-                    <input 
+                    <form
+                        onSubmit={handleSubmit} 
+                    >
+                    <input
+                        value={username.name} 
                         type="text" 
                         name="name"
                         placeholder="Usuário Github"
                         className="input-profile-search" 
+                        onChange={handleOnChange}
                     />
                     <Button 
-                        title="Encontrar"    
+                        title="Encontrar"
                     />
+                    </form>
                 </div>
             </CardBase>
         </div>
 
+        {user && 
         <div className="card-base-profile-info-container">
             <CardBase className="card-base info-content">
-                <div className="profile-data">
-                    <div>
-                        <img 
-                            src="https://avatars2.githubusercontent.com/u/43610369?s=460&v=4" 
-                            alt="profile-img"
-                            className="profile-img"/>
-                    </div>
-                    <div className="card-base-profile-info">
-                        <div className="upper-data">
-                            <UpperProfileData
-                                name="Repositórios públicos"
-                                value={62}
-                            />
-                            <UpperProfileData
-                                name="Seguidores"
-                                value={127}
-                            />
-                            <UpperProfileData
-                                name="Seguindo"
-                                value={416}
-                            />
+                        <div className="profile-data">
+                            <div>
+                                {isLoading ? <ImageLoader /> : (
+                                <img
+                                    src={user.avatar_url}
+                                    alt={user.login}
+                                    className="profile-img" />)}
+                            </div>
+                            <div className="card-base-profile-info">
+                                {isLoading ? <InfoLoader/> : (
+                                <>
+                                    <div className="upper-data">
+                                        <UpperProfileData
+                                            name="Repositórios públicos"
+                                            value={user.public_repos} />
+                                        <UpperProfileData
+                                            name="Seguidores"
+                                            value={user.followers} />
+                                        <UpperProfileData
+                                            name="Seguindo"
+                                            value={user.following} />
+                                    </div>
+                                    <div className="profile-field-container">
+                                        <h3 className="profile-field-title">Informações</h3>
+                                        <ProfileFieldData
+                                            name="Empresa"
+                                            content={user.company} />
+                                        <ProfileFieldData
+                                            name="Website/Blog"
+                                            content={user.blog} />
+                                        <ProfileFieldData
+                                            name="Localidade"
+                                            content={user.location} />
+                                        <ProfileFieldData
+                                            name="Membro desde"
+                                            content={dayjs(user.created_at).format('DD/MM/YYYY')} />
+                                    </div>
+
+                                   
+                                </>
+                                )}
+                            </div>
                         </div>
-                        <div className="profile-field-container">
-                            <h3 className="profile-field-title">Informações</h3>
-                            <ProfileFieldData 
-                                name="Empresa"
-                                content=""
-                            />
-                             <ProfileFieldData 
-                                name="Website/Blog"
-                                content=""
-                            />
-                             <ProfileFieldData 
-                                name="Localidade"
-                                content=""
-                            />
-                             <ProfileFieldData 
-                                name="Membro desde"
-                                content=""
-                            />
-                        </div>
-                    </div>
-                </div>
-                <Button 
-                        title="Ver perfil"    
-                />
+                        {!isLoading && 
+                        <Button
+                            title="Ver perfil" 
+                        />
+                        }
             </CardBase>
         </div>
+    }
     </>
-);
+    );
+}
 
 export default Search;
